46bd1171ed78b19b6bb13670271fea660ccd7103
William Kamp <manimaul@gmail.com>
2025-09-05 06:43:04 -0700

    import from njord

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    kotlin("multiplatform") version "2.2.0"
+    kotlin("plugin.serialization") version "2.2.0"
+}
+
+
+group = "io.madrona.geojson"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(17)
+
+    jvm {
+    }
+
+    js {
+        browser()
+        useEsModules()
+    }
+
+    val hostOs = System.getProperty("os.name")
+    val isArm64 = System.getProperty("os.arch") == "aarch64"
+    val isMingwX64 = hostOs.startsWith("Windows")
+    when {
+        hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
+        hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
+        hostOs == "Linux" && isArm64 -> linuxArm64("native")
+        hostOs == "Linux" && !isArm64 -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.0")
+                api("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
